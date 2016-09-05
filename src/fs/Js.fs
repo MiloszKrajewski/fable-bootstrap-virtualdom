6af4c8b02804bec3e42788e64e76d81fc8b4fd1b
Milosz Krajewski <miloszkrajewski@o2.pl>
2016-09-05 22:26:50 +0100

    some helper functions

--- src/fs/Js.fs
+++ src/fs/Js.fs
@@ -4,7 +4,11 @@ open Fable.Core
 open Fable.Import.Browser
 open System
 
-module Js = 
+module Js =
+    let inline empty () = createEmpty ()
+    let inline newmap fields = createObj fields
+    let inline newobj o args = createNew o args
+
     module Option = 
         let def v o = defaultArg o v
         let alt a o = match o with | None -> a | _ -> o
